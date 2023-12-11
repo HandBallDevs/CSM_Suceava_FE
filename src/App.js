a8e1d2905c6cae6874b7825cdd8980d40cb85644
EdySch <eduard.schipor@student.usv.ro>
2023-12-11 11:38:31 +0200

    CSU-29 AdminMeciuri Page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // App.js
 import React from "react";
-//import Layout from './layout';
-//import Acasa from "./Acasa";
-//import Noutati from "./Noutati";
-//import Contact from "./Contact";
-//import Meniusus from "./Meniusus";
-//import Meniujos from "./Meniujos";
+import Layout from './layout';
+import Acasa from "./Acasa";
+import Noutati from "./Noutati";
+import Contact from "./Contact";
+import Meniusus from "./Meniusus";
+import Meniujos from "./Meniujos";
 import Admin from "./Admin";
 import AdminStiri from "./AdminStiri";
-
+import AdminMeciuri from "./AdminMeciuri";
 //Am updatat codul cu fisierele meniujos si sus care practic pun tot ce este sus si tot ce este jos si nu editam doar mijlocul,
 //pentru ca va fi complicat cand vom avea functionalitati.Am updatat eu tot codul si arata mai bine acum.Sry daca ai facut modificari intre timp
 
 function App() {
-  return <AdminStiri />;
+  return <AdminMeciuri />;
 }
 
 export default App;
