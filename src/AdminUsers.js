f5cde75e07e3166b1f6935727123b2796c03626a
EdySch <eduard.schipor@student.usv.ro>
2024-01-11 18:42:16 +0200

    Pagina AdminUsers

--- src/AdminUsers.js
+++ src/AdminUsers.js
@@ -6,6 +6,9 @@ import AdminMenuImage from "./imagini/AdminMenuImage.png";
 import AdminWorkSpaceImage from './imagini/AdminWorkSpaceImage.png';
 import { useNavigate,Link } from "react-router-dom";
 import AdminUsersImage from  "./imagini/AdminUsersImage.png";
+import EditImage from "./imagini/EditImage.png";
+import DeleteImage from "./imagini/DeleteImage.png";
+
 const Admin = () => {
   return (
     <div className="app-container">
@@ -47,6 +50,46 @@ const Admin = () => {
             <input type="text" className="workspace-inputs-ADMUsers" placeholder="Search...." />
 
           </div>
+
+          <div className="workspace-row-ADMUsers2">
+            <div className="workspace-collumn-ADMUsers ">
+              <label className="label-list-ADMUsers">ID</label>
+              <label className="label-exemple-ADMUsers">#123456</label>
+              <label className="label-exemple-ADMUsers">#123457</label>
+            </div>
+            <div className="workspace-collumn-ADMUsers ">
+              
+              <label className="label-list-ADMUsers">Adresa email</label>
+              <label className="label-exemple-ADMUsers">user1@yahoo.com</label>
+              <label className="label-exemple-ADMUsers">user2@email.com</label>
+            
+            </div>
+            <div className="workspace-collumn-ADMUsers ">
+              
+              <label className="label-list-ADMUsers">Data</label>
+              <label className="label-exemple-ADMUsers">01/01/2023</label>
+              <label className="label-exemple-ADMUsers">02/01/2023</label>
+            
+            </div>
+            <div className="workspace-collumn-ADMUsers ">
+              
+              <label className="label-list-ADMUsers">Actiuni</label>
+              <div className="actions-ADMUsers3">
+                 <img src={EditImage} alt="" className="Action-image-ADMUsers" />
+                 <img src={DeleteImage} alt="" className="Action-image-ADMUsers" />
+               </div>
+              <div className="actions-ADMUsers3">
+                <img src={EditImage} alt="" className="Action-image-ADMUsers" />
+                <img src={DeleteImage} alt="" className="Action-image-ADMUsers" />
+              </div>
+            
+            </div>
+
+
+          </div>
+
+
+
         </div>
       </div>
 
